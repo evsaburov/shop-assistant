e7718c961a17385d3542f3036e01dc64ec0ed8fa
evgeniy <evs.zlat@gmail.com>
2022-10-03 00:26:08 +0500

    add user middleware

--- src/telegram-bot/types.ts
+++ src/telegram-bot/types.ts
@@ -1,5 +1,9 @@
-export enum keyboardCommands {
+export enum KeyboardCommands {
 	catalog = 'Каталог товаров',
 	settings = 'Настройки',
 	card = 'Карта лояльности',
+}
+
+export enum Text {
+	hello = 'Привет,Добро пожаловать в Магазин',
 }
